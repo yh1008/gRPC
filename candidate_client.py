20576a0c3c790f389c0901941264188d0e9304a1
yh1008 <ye.hua@nyu.edu>
2015-11-25 23:47:03 -0500

    candiate_client.py

---
+++ candidate_client.py
@@ -0,0 +1,12 @@
+from grpc.beta import implementations
+
+import debate_pb2
+_TIMEOUT_SECONDS = 10
+
+def run():
+        channel = implementations.insecure_channel('localhost', 80000)
+        stub = debate_pb2.beta_create_Candidate_stub(channel)
+        response = stub.Answer(debate_pb2.AnswerRequest(question="what is your political opinion on poverty", timeout=10),_TIMEOUT_SECONDS)
+        print "Candiate client received: " + response.answer
+if __name__ == '__main__':
+  run()
