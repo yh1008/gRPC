d0bbeb8a45e26548b32b6fdb6f5bf70cf1668e69
yh1008 <ye.hua@nyu.edu>
2015-12-17 22:41:34 -0500

    Update candidate_client.py

--- candidate_client.py
+++ candidate_client.py
@@ -4,7 +4,7 @@ import debate_pb2
 _TIMEOUT_SECONDS = 10
 
 def run():
-        channel = implementations.insecure_channel('localhost', 80000)
+        channel = implementations.insecure_channel('localhost', 8080)
         stub = debate_pb2.beta_create_Candidate_stub(channel)
         response = stub.Answer(debate_pb2.AnswerRequest(question="what is your political opinion on poverty", timeout=10),_TIMEOUT_SECONDS)
         print "Candiate client received: " + response.answer
